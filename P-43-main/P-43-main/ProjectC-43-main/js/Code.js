996b18948594d0799fec82e20257deaea3bfec6f
Aarav-op <76724504+Aarav-op@users.noreply.github.com>
2021-06-26 12:47:41 +0530

    Add files via upload

---
+++ P-43-main/P-43-main/ProjectC-43-main/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+     fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+   
+    fill("white")
+    textSize(15)
+    text(" B S A E T A A D", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information!!", 100,270)
+   
+    fill("white")
+    textSize(15)
+    text(" U F N T C O I N ", 700,150)
+    fill("lightblue")
+    text("Hint: Performes a particular task !! !!", 700,170)  
+}
+
